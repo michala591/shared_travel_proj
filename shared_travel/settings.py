f8169d7613a491c7e5c3b4a1b7e3de1ba87f3423
micahla591 <a0548562302@gmail.com>
2025-01-12 16:31:38 +0200

    allow front in render

--- shared_travel/settings.py
+++ shared_travel/settings.py
@@ -62,7 +62,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
 "https://shared-travel-react.onrender.com/"
 ]
-ALLOWED_HOSTS = ["shared-travel-react.onrender.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["shared-travel-react.onrender.com","shared-travel-proj.onrender.com", "127.0.0.1", "localhost"]
 
 CORS_ALLOWED_ORIGINS = [
      "https://shared-travel-react.onrender.com" # Allow your frontend to make requests
